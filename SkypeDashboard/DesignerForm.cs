566c69ef16e97b7798eb3b2e16cf489c2f63375b
Sshur <sshur@mail.ru>
2015-02-03 14:44:30 +0300

    Initial state

---
+++ SkypeDashboard/DesignerForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SkypeDashboard {
+    public partial class DesignerForm : DevExpress.XtraEditors.XtraForm {
+        public DesignerForm() {
+            InitializeComponent();
+        }
+
+        void DesignerForm_Load(object sender, EventArgs e) {
+            dashboardDesigner1.LoadDashboard("..\\..\\Dashboard\\Skype.xml");
+        }
+
+    }
+}
